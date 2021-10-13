0ae1f95bb4b05c3fb12b42fcd9170176b652ac4b
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-13 19:49:51 +0530

    initial_commit

---
+++ src/Album.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    
+}
---
+++ src/Song.java
@@ -0,0 +1,29 @@
+public class Song {
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+    // empty constructor
+    public Song() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
